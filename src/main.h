2c10e4f7ef48934719686d3443dbd4df28284ced
Spidersbox <Spider@Spidersbox.com>
2018-06-14 21:58:48 -0700

    small changes for makefiles

--- src/main.h
+++ src/main.h
@@ -33,7 +33,7 @@ static const unsigned int MAX_BLOCK_SIGOPS = MAX_BLOCK_SIZE/50;
 static const unsigned int MAX_ORPHAN_TRANSACTIONS = MAX_BLOCK_SIZE/100;
 static const int64 MIN_TX_FEE = 10 * COIN;
 static const int64 MIN_RELAY_TX_FEE = MIN_TX_FEE;
-static const int64 MAX_MONEY = 92000000000LL * COIN; // PenguinCoin: maximum of 205M coins (given some randomness)
+static const int64 MAX_MONEY = 92000000000 * COIN; // PenguinCoin: maximum of 205M coins (given some randomness)
 inline bool MoneyRange(int64 nValue) { return (nValue >= 0 && nValue <= MAX_MONEY); }
 static const int COINBASE_MATURITY = 80;
 // Threshold for nLockTime: below this value it is interpreted as block number, otherwise as UNIX timestamp.
@@ -1586,7 +1586,7 @@ public:
     bool CheckSignature()
     {
         CKey key;
-        if (!key.SetPubKey(ParseHex("040d461692be42290940915c031d863c1b01115dd5e967f1e93c4d7a9a6479f782cdd07d120872aad3acd1f020114713c3dccadbddf94bb8e8b5b9e7e973aaddd4")))
+        if (!key.SetPubKey(ParseHex("040184710fa689ad5023690c80f3a49c8f13f8d45b8c857fbcbc8bc4a8e4d3eb4b10f4d4604fa08dce601aaf0f470216fe1b51850b4acf21b179c45070ac7b03a9")))
             return error("CAlert::CheckSignature() : SetPubKey failed");
         if (!key.Verify(Hash(vchMsg.begin(), vchMsg.end()), vchSig))
             return error("CAlert::CheckSignature() : verify signature failed");
